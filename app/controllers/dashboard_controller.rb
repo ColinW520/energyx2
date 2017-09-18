33d51c61fa0885d7516d52a26a041f61c3c56b13
ColinW520 <colinhagan@gmail.com>
2017-09-18 15:27:03 -0500

    show participant data!

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
   def show
-    @p_data = Submission.where(submissions: {created_at: 1.month.ago..Time.now}).joins(:participant).group("participants.name").count.sort_by {|k,v| v}.reverse
+    @s_data = Submission.where(submissions: {created_at: 1.month.ago..Time.now}).joins(:participant).group("participants.name").count.sort_by {|k,v| v}.reverse
     @page_data = Ahoy::Event.where(user_id: nil).where(time: 1.month.ago..Time.now).group(:name).count.sort_by {|k,v| v}.reverse
     @city_data = Visit.group(:city).where(user_id: nil).where.not(city: nil).where(started_at: 1.month.ago..Time.now).count.sort_by {|k,v| v}.reverse
+    @p_data = Participant.order(created_at: :desc).limit(10)
   end
 end
