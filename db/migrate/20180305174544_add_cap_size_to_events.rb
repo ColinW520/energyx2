d87428899d52908e48ef30dfa571cd30ee53d270
ColinW520 <colinhagan@gmail.com>
2018-03-05 12:59:45 -0600

    legacy support for this feature would be good.

--- db/migrate/20180305174544_add_cap_size_to_events.rb
+++ db/migrate/20180305174544_add_cap_size_to_events.rb
@@ -1,5 +1,5 @@
 class AddCapSizeToEvents < ActiveRecord::Migration[5.0]
   def change
-    add_column :events, :cap_size, :integer
+    add_column :events, :cap_size, :integer, default: 50
   end
 end
