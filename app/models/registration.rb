13ddad60b0587dd4c9a2140e4ed58ef1400b8fa3
ColinW520 <colinhagan@gmail.com>
2018-12-15 10:05:31 -0600

    remove the charge creation steps from Registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -7,29 +7,6 @@ class Registration < ApplicationRecord
     Stripe::Charge.retrieve(self.stripe_charge_id)
   end
 
-  def create_charge(params)
-    raise 'Charge has already been created for this registration.' if self.is_paid?
-
-    params = params.with_indifferent_access
-
-    customer = Stripe::Customer.create(
-      :email => self.email,
-      :source  => params[:stripe_token]
-    )
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => self.event.price_in_cents,
-      :description => "ENERGYX #{self.event.name}",
-      :currency    => 'usd'
-    )
-
-    self.stripe_customer_id = customer.id
-    self.stripe_charge_id = charge.id
-    self.is_paid = true if customer && charge
-    self.save!
-  end
-
   def self.to_csv
     header = ['Event Name', 'First Name', 'Last Name', 'Phone', 'Shirt Size']
     attributes = []
@@ -39,7 +16,7 @@ class Registration < ApplicationRecord
 
       all.each do |t|
         csv << attributes.map do |attr|
-          attr == 'tags' ? t.tags.pluck(:name).split(',').join('|') : t.send(attr)
+          t.send(attr)
         end
       end
     end
